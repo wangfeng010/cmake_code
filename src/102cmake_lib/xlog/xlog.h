1b0445df518287a393dddb8c82652a5b6537f898
wangfeng010 <1977513290@qq.com>
2024-04-22 03:44:59 +0800

    Building a dynamic library using MSVC on Windows

--- src/102cmake_lib/xlog/xlog.h
+++ src/102cmake_lib/xlog/xlog.h
@@ -7,10 +7,14 @@
 //__declspec(dllexport) 导出XLog类的函数到1ib文件中
 //xlog库文件调用 d11export
 //test_xlog调用 dllimport
-#ifdef xlog_EXPORTS
-    #define XCPP_API __declspec(dllexport) //库项目调用
+#ifndef _WIN32
+    #define XCPP_API
 #else
-    #define XCPP_API __declspec(dllexport) //调用库项目调用
+    #ifdef xlog_EXPORTS
+        #define XCPP_API __declspec(dllexport) //库项目调用
+    #else
+        #define XCPP_API __declspec(dllexport) //调用库项目调用
+    #endif
 #endif
 class XCPP_API XLog
 {
